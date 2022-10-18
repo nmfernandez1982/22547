af3f03899e93f651b1555c8bbcfd529e0b382482
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2022-10-18 06:37:46 -0300

    Clase23

---
+++ Clase23/js/scripts.js
@@ -0,0 +1,14 @@
+function cambiar()
+{
+    var aux=document.getElementById("claseDiv");
+    aux.classList.add("nuevo");
+}
+
+
+function cambiar2()
+{
+    var aux=document.getElementById("claseB");
+    aux.classList.add("alert alert-danger");
+}
+
+
